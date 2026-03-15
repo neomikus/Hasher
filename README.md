7e3265b378ebe4d1c807a184d27e12126cd23610
princess-mikus <kiankocasaubon01@gmail.com>
2026-03-15 17:32:44 +0100

    Added copy to clipboard functionality, and redone argument parsing with argparse

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 <h1>#️ Hasher #️</h1>
 Small python script to hash a password
 
+<h2>⚙️ Dependencies ⚙️</h2>
+
+- Python3
+
+Optional:
+- <a href="https://pypi.org/project/pyperclip/">Pyperclip</a> (for copy to clipboard functionality)
+- Any copy/paste mechanism (like xclip, xselect, wl-clipboard) in the OS (for copy to clipboard functionality)
+
 <h2>🛠️ Usage 🛠️</h2>
 
 1. **Clone the repository:**
@@ -11,7 +19,9 @@ Small python script to hash a password
 
 2. **Execute the script:**
    ```bash
-   python3 main.py [algorithm]
+   python3 main.py [--algorithm/-a] [--clipboard/-c]
    ```
 
 Where algorithm can be any algorithm supported by the hashlib library (case indifferent). If not set, defaults to sha256
+
+If the clipboard flag is set, the program will copy the resulting hashed password into your clipboard (pyperclip needed, see dependencies)
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import hashlib
 import getpass as gp
 import sys
+import argparse
+
+def init_argparse():
+    parser = argparse.ArgumentParser(
+        prog="Hasher",
+        description="Password hasher"
+    )
+    parser.add_argument("--algorithm", "-a", choices=hashlib.algorithms_available, default="sha256", type=str.lower, help="Selected hash algorithm")
+    parser.add_argument("--clipboard", "-c", action="store_true", default=False, help="If this flag is present, output will be directly copied to clipboard")
+    return (parser)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        algorithm = "sha256"
-    else:
-        algorithm = sys.argv[1].lower()
-    if algorithm not in hashlib.algorithms_available:
-        print("Error: Not a valid algorithm", file=sys.stderr)
-        sys.exit(1)
+    parser = init_argparse()
+    args = parser.parse_args()
+    algorithm = args.algorithm.lower()
     message = hashlib.new(algorithm)
     try:
         password = gp.getpass(prompt="Insert password: ")
@@ -20,4 +26,17 @@ if __name__ == "__main__":
         print("Error: Empty password", file=sys.stderr)
         sys.exit(1)
     message.update(password.encode())
-    print(message.hexdigest())
+    if (args.clipboard):
+        try:
+            import pyperclip
+        except ModuleNotFoundError:
+            print("Error: optional dependency pyperclip not installed", file=sys.stderr)
+            sys.exit(1)
+        try:
+            pyperclip.copy(message.hexdigest())
+        except pyperclip.PyperclipException as e:
+            print("pyperclip exception:", e, file=sys.stderr)
+        else:
+            print("Copied to clipboard!", file=sys.stderr)
+    else:
+        print(message.hexdigest())
